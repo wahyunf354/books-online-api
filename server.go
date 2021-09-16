f07673eb5904909b939333323fff98d0755805b3
wahyunf354 <wahyunurfadillah313@gmail.com>
2021-09-16 17:43:53 +0700

    berhasil implementasi echo

--- server.go
+++ server.go
@@ -6,10 +6,32 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+type User struct {
+	Name    string `json:"name"`
+	Email   string `json:"email"`
+	Address string `json:"address"`
+}
+
+type BaseResponse struct {
+	Code    int         `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
+func GetUserController(c echo.Context) error {
+	users := User{"Wahyu", "wahyo@hh.com", "malang"}
+
+	return c.JSON(http.StatusOK, BaseResponse{
+		Code:    http.StatusOK,
+		Message: "Success",
+		Data:    users,
+	})
+}
+
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello Word")
-	})
-	e.Logger.Fatal(e.Start(":1323"))
+
+	e.GET("v1/users", GetUserController)
+
+	e.Start(":8000")
 }
