a1e1731e73d3689b1d30aa99deffe77723af7be3
wahyunf354 <wahyunurfadillah313@gmail.com>
2021-09-19 09:20:40 +0700

    fix bug dari struct UserRegister Kurang " di Birth

--- models/users/user_register.go
+++ models/users/user_register.go
@@ -5,7 +5,7 @@ type UserRegister struct {
 	LastName     string `json:"last_name"`
 	Email        string `json:"email"`
 	Role         int8   `json:"role"`
-	Birth        string `json:"birth`
+	Birth        string `json:"birth"`
 	Gender       string `json:"gender"`
 	Password     string `json:"password"`
 	ConfPassword string `json:"confpassword"`
