36200c80be59a1e7be6184885a8bd7661fbcbc06
wahyunf354 <wahyunurfadillah313@gmail.com>
2021-09-26 15:51:13 +0700

    fix bug get google bug invalid link

--- drivers/thirdparts/google_books/google_books.go
+++ drivers/thirdparts/google_books/google_books.go
@@ -14,13 +14,14 @@ type ThirtPartGoogleBooks struct {
 
 func NewGoogleBooksAPIThirtPart() google_books.ThirdPartyGoogleBooks {
 	return &ThirtPartGoogleBooks{
-		LinkAPI: "https://www.googleapis.com/book_types/v1/volumes?startIndex=%v&maxResults=%v&orderBy=relevance&filter=free-ebooks&q=%v",
+		LinkAPI: "https://www.googleapis.com/books/v1/volumes?startIndex=%v&maxResults=%v&orderBy=relevance&filter=free-ebooks&q=%v",
 	}
 }
 
 func (gb *ThirtPartGoogleBooks) SearchBooks(ctx context.Context, keyword string, startIndex int, maxResult int) ([]google_books.Domain, error) {
 	var googleBooks GoogleBooks
 	link := fmt.Sprintf(gb.LinkAPI, startIndex, maxResult, keyword)
+	fmt.Println(link)
 	result, err := http.Get(link)
 
 	if err != nil {
@@ -31,6 +32,7 @@ func (gb *ThirtPartGoogleBooks) SearchBooks(ctx context.Context, keyword string,
 	body := result.Body
 
 	if err := json.NewDecoder(body).Decode(&googleBooks); err != nil {
+		fmt.Println(err)
 		return []google_books.Domain{}, err
 	}
 
