b6b6813061f9afd91b320c255c1c1086d9025da2
wahyunf354 <wahyunurfadillah313@gmail.com>
2021-10-04 18:37:36 +0700

    test user business cover 89%

---
+++ business/book_types/book_types_test.go
@@ -0,0 +1,1 @@
+package book_types_test
--- mvc/models/books/category_db.go
+++
@@ -1,9 +0,0 @@
-package books
-
-import "gorm.io/gorm"
-
-type Category struct {
-	gorm.Model
-	Title string  `gorm:"not null"`
-	Books []*Book `gorm:"foreignKey:CategoryId;references:ID"`
-}
